a357b55c544144c3c71c58d5b478ca055aed38f8
Noversky <veresoa@bk.ru>
2025-06-02 11:19:54 +0300

    Add files via upload

---
+++ Noversky Antivirus12.bat
@@ -0,0 +1,13 @@
+@echo off
+color b
+@echo Noversky Antivirus
+@echo Start Taskmgr.exe?
+pause
+Start Taskmgr.exe
+@echo Start Rededit.exe?
+pause
+Start Regedit.exe
+@echo Start Resmon.exe?
+pause
+Start Resmon.exe
+
